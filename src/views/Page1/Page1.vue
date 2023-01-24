6402ca081ed4daa2b824e1e04cbc77e23d74d6fd
oldking139 <oldking139@foxmail.com>
2023-01-24 15:32:42 +0000

    fix: 修改末行

--- src/views/Page1/Page1.vue
+++ src/views/Page1/Page1.vue
@@ -41,7 +41,7 @@ const download = () => {
       <div class="main-text">
         这里是乐华娱乐旗下虚拟偶像女团[EOE组合]的非官方二创社区平台APP
         本APP非商业用途，仅提供EOE及相关社区服务。<br><br>
-        不开心就来EOEFans！在这里，没有人知道你的身份。在EOEFans，没有标签，只有熟人。在EOEFans，尽情的释放自己。在EOEFans，遇见懂你的二创。 <br><br> 
+        不开心就来EOEFans！在这里，没有人知道你的身份。在EOEFans，没有标签，只有熟人。在EOEFans，尽情的释放自己。在EOEFans，遇见懂你的二创。<br> 
         Welcome to eoe world.     </div>
       <section class="button-section">
         <div class="download-button__base" @click="download">前往下载</div>
